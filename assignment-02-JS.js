18faaf3e2e8e29460be9909a72c073e6c88ad0be
TSN Student <student@ulem.org>
2018-05-07 08:59:37 -0400

    updated and removved some files

--- assignment-02-JS.js
+++ assignment-02-JS.js
@@ -8,7 +8,7 @@
 
 
 
-/*
+/*				
 ########################################################################
 [  Question -01 ]
 Write the function printInt(n) taking one parameter n and print all 
@@ -43,11 +43,6 @@ printInt(0)
 #########################################################################
 #########################################################################
 #########################################################################
-*/
-
-
-
-/*
 ########################################################################
 [  Question -02 ]
 
@@ -84,10 +79,6 @@ printIntRev(5)
 #########################################################################
 #########################################################################
 #########################################################################
-*/
-
-
-/*
 ########################################################################
 [  Question -03 ]
 
@@ -107,20 +98,22 @@ Write the function [checkInput(x)]
 #########################################################################
 */
 
+
+
 function checkInput(x) 
 	{
-
-			if (typeof x === "undefined") {
-				document.write("-1");
+			if (typeof x === "number" || typeof x ===  "string"|| typeof x ===  "boolean") {
+				document.write(typeof x);
+				console.log(typeof x);
 			}
 			else{
-				document.write(typeof x);
+				
+				document.write("-1");
+				consol.log('-1');
 			}
-
 	}
 
-
-checkInput('test')
+checkInput(34.32)
 
 
 
@@ -129,8 +122,6 @@ checkInput('test')
 #########################################################################
 #########################################################################
 #########################################################################
-*/
-/*
 ########################################################################
 [  Question -04 ]
 
@@ -147,27 +138,26 @@ Write a function simpleEvenAdding(num)
 #########################################################################
 */
 
-
 function simpleEvenAdding(num)
 	{
 		var even_num = 0;
 		var i =0;
 		var answer = 0;
-		
+
 		for(i=2; i <= num; i= i+2) 
 			{
 				even_num = i+2 ;
-				// console.log(i);
 				answer = i + answer;
 			}
-
-
-				document.write('Final answer  = ' + answer);
-
+				document.write('Answer  = ' + answer);
+				console.log('Answer  = ' + answer);
 	}
 
 
-simpleEvenAdding(num)
+// simpleEvenAdding(num)
+simpleEvenAdding(10)
+
+
 
 
 
@@ -176,9 +166,6 @@ simpleEvenAdding(num)
 #########################################################################
 #########################################################################
 #########################################################################
-*/
-
-/*
 ########################################################################
 [  Question -05 ]
 
@@ -199,12 +186,455 @@ Write a function letterCapitalize()
 function letterCapitalize(str)
 {
 
- return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+ return str.replace(/\w\S*/g, function(txt)
+ 		{
+ 			return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+ 		});
 }
 
-document.write(letterCapitalize('tell me whats there to do in town'));
+// document.write(letterCapitalize('tell me whats there to do in town'));
+
+
+/*
+		/\w\S/g
+		/  				regular expression
+		\w   			looks for word chr in the string
+		\S 	with star   any chr that is not a whitespace
+		\g  			selects each instance
+
+
+*/
+
+document.write(letterCapitalize('ok here we go on a trip'));
+
+/*
+#########################################################################
+#########################################################################
+#########################################################################
+#########################################################################
+[  Question 6 ]
+
+	[_]  Wtire a Function simpleReverse(str)
+
+			[_]  Taking a string and return the string in reverse order
+
+			[EXAMPLEs]
+						simpleReverse('hello')  =>  'olleh'
+						simpleReverse('I Love Code')  =>  "edoC evoL I"
+
+				[Hint]   Think of how you can loop through a string
+				           or array for the charachers backwords to 
+				           product a new string.
+
+######################################################################### */
+
+
+function simpleReverse(str)
+		{
+			
+			// varables
+			var str_loop = 0;
+			var str_count = str.length;
+
+			document.write("<br> Origintial String = " + str);
+			document.write("<br>" );
+			document.write("<br> New String = ");
+			
+						// THIS SPLITS EVERYTHING AnD COMMA DELIMITED
+			var str_split = str.split("");     
+  			var str_split_array = new Array(str_split)
+
+				 for ( var m = str_count -1; m >=0 ; m = m -1)  // m = 
+					{  
+								document.write(str[m]); 
+								console.log(str[m]);
+ 					} 
+
+	}
+
+
+simpleReverse(' its time for MSIMBO');
+
+
+/*
+#########################################################################
+#########################################################################
+#########################################################################
+#########################################################################
+[  Question 7 ]
+
+	[_]  Write a function findDiff(arr)
+
+		[_]  taking an array of numbers as parameter and return
+		 the difference between the max number and the min number
+		 	(max - min)
+
+		 EXAMPLEs ;
+
+		 		findDiff( [1,2,4,6,20,31] )  => 19 (because 20-1 =19)
+
+		 		findDiff([24,22,23,22,24])  => 2  (because 24-22 = 2)
+
+		 		findDiff([1,1,1,1,1])   => 0
+
+		 		findDiff([1])  => 0
+
+		 		findDiff([])   => 0
+				
+######################################################################### */
+
+var arr_math = 0;
+var arr_counter = 0;
+var arr_max = 0;
+var m_max = 0;
+var m_min = 0;
+var m_diff = 0;
+
+function findDiff(arr) 
+	{
+		document.write("<br>" );
+		document.write('Entered Array = [Below]' + '<br>' + arr);
+		
+		var m_max = Math.max.apply(null, arr);
+		var m_min = Math.min.apply(null, arr);
+
+		document.write("<br>" );
+		document.write('Max Array # =  ' + m_max);
+
+		document.write("<br>" );
+		document.write('Min Array # =  ' + m_min);
+
+		var m_diff = m_max - m_min;
+		document.write("<br>" );
+		document.write('Difference Between =  ' + m_diff);
+		//_________________________________________________
+
+	}
+
+findDiff([23,114,345,23,445,677,654,42,55,11])
+
+
+
+/*
+#########################################################################
+#########################################################################
+#########################################################################
+#########################################################################
+[  Question 8 ]
+
+	[_]  Write a function timeConvert(num)
+
+		[_]  taking a number as a paramenter and retur the number of hours and minutes the parameter converts to.
+
+		[_]  Seperate the number of hours and minutes with a colon.
+
+		EXAMPLE :
+
+			timeConvert(01)   =>  1:1
+			timeConvert(63)   =>  1:3
+			timeConvert(120)  =>  2:0
+			timeConvert(59)   =>  0:59
+
+			HINTs :  dividing and modulo the number 60
+				
+######################################################################### */
+
+
+	function timeConvert (num){
+					
+					var result_l = parseInt(num / 60);
+					var result_r = num % 60;
+					var result = result_l + " : " + result_r
+					return result;
+				}
+
+
+document.write(timeConvert(150));
+
+
+
+/*
+#########################################################################
+#########################################################################
+#########################################################################
+#########################################################################
+[  Question 9 ]
+
+	[_]  Write a function findStr(str, long)
+
+		[_]  taking 2 strings as parameters
+		[_]  RETURN how many str you can fins in long string.
+
+		[_]  Assuming that str is not empty !
+
+		EXAMPLE:
+				findStr('hi', 'dasdhidasdasfehisdkskhi')  => 3
+				findStr('o','daodo' )   => 2
+				findStr("ha" , "abcode")  => 0
+
+				
+######################################################################### */
+
+
+
+function findStr(str, long) 
+	{
+
+		var tgt = str.toLowerCase();
+		var found = 0;
+	
+		var str = "";
+	
+
+		var crap = long.toLowerCase();
+			
+		var crap_found = 0;
+		var counter = 0
+		
+	
+
+	if (long.includes(str))
+
+			{
+
+	
+
+				for(var i = 0 ; i <= crap.length ; i++)
+					{
+
+						get_crap = crap.indexOf(tgt) ;
+
+							if (long.includes(str))
+								{
+									
+									var counter = counter+1		
+
+									
+										var crap = crap.slice(get_crap +1);
+										
+
+										i = get_crap -i;
+										
+
+								}
+							else{
+								return
+							}
+
+							
+
+					
+
+
+					}
+			}
+		else{
+			return;
+		}
+
+
+
+	document.write("<br>  Counter  = " + counter);
+
+				// document.write("<br>" );
+				// var res = long.match(str);
+				// // var res = long.match(/ain/gi);
+				// document.write(" match :" + long.search(tgt));
+
+				// document.write("<br>" );
+				// document.write(" search :" + long.search(str));
+
+				// document.write("<br>" );
+				// document.write(" includes :" + long.includes(str)) ;  // boolean true/false
+				// 													//  use as Conditional test
+
+				// document.write("<br>" );
+				// document.write(" match :" + long.match(str)) ;  // retruns  cpr
+
+
+	}
+
+
+// findStr("str", "long");
+findStr("ok", "ok here we go ok get er ok done ok");  // 2 copies
+// findStr("", "");
 
 
 
 
+/*
+#########################################################################
+#########################################################################
+#########################################################################
+#########################################################################
+[  Question 10 ]
 
+	[_]  Write a function selfDividingNumbers(left, right)
+
+		[_]  taking two(2) numbers bound as parameters
+		[_]  RETURN an array[] of every possible self dividinG number
+		          between them
+
+		[_]  INCLUDE bounds
+
+		EXAMPLE:
+				128 is a self dividing number because ;
+						128 % 1 == 0
+						128 % 2 == 0
+						128 % 8 == 0
+
+				( 1 , 22 )  =>  [1,2,3,4,5,6,7,8,9,11,12,15,22]
+
+				( 1 , 10 )  =>  [1,2,3,4,5,6,7,8,9]
+
+				( 12 , 21 ) =>  [12,15]
+
+			HINT  ;  a self-dividing number is a number that is divided b
+			      y every digit it containes.
+				
+				
+######################################################################### */
+
+
+
+      // ================================================
+      // 			SUB FUNCTION TO DO THE MATH
+      // ================================================
+function selfDiv(t_arr)
+      {
+
+        var parc_arr  = t_arr.toString();
+        var arr_str = parc_arr.split("");
+        var div_Numbers = [];
+        
+        		// LOOPS THROUGH EACH DIGET
+             for (var m = 0 ; m < arr_str.length ; m++)
+                      {
+                        var divCheck = t_arr % arr_str[m];
+
+                        // CHECKS ID ITS = 0
+							if (divCheck == 0)
+	                             {
+	                                 var parc_flag = true;  
+	                             }
+	                        else
+	                             {
+	                                 var parc_flag = false;
+	                                  return
+	                             }
+                        var divResult = parc_flag;
+                         
+                        }
+
+        div_Numbers.push(t_arr);
+        document.write('<br>' +div_Numbers  );
+        console.log(div_Numbers );
+
+      }
+
+      // =================================================
+      // MAIN FUNCTION TO LOOP THROUGH THE NUMBERS GIVIN
+      // =================================================
+function selfDividingNumbers(left, right)
+           {
+           	var div_rtn = [];
+            var i_arr = [];
+            var i_arr_s = [];
+
+            document.write('Numbers entered are    LEFT:' + left + '  RIGHT:' + right);
+            document.write('<br>');
+            console.log('Numbers entered are    LEFT:' + left + '  RIGHT:' + right);
+            document.write('Self Deviding Numbers are  ');
+            console.log('Self Deviding Numbers are  ');
+
+            		// LOOPS STARTS AT # LEFT  -TO- THE RIGHT #
+                       for(var i = left ; i <= right ; i++)
+                           {
+                               var i_arr = i;  
+                               // CALL THE SUB FUNCTION
+                               selfDiv(i_arr);
+                           }
+
+           }
+
+
+// selfDividingNumbers( left, right);
+selfDividingNumbers( 100, 200);
+
+
+/*
+#########################################################################
+#########################################################################
+#########################################################################
+#########################################################################
+[  Question Extra Credit ]
+
+	[_]  Write a function moveZeros(nums)
+
+		[_]  taking an array[] of numbers
+		[_]  MOVE all 0 (zeros) to the end of it
+		[_]  While maintaining the relative order of the non-zero elements.
+
+	EXAMPLE : 
+
+		moveZeros([0,1,3,12])    			=>  [1,3,12,0,0,]
+		moveZeros([1,2,0,0,2,312,33,00])    =>  [1,2,312,33]
+		moveZeros([0,0,0]) 					=>  [0,0,0,]
+		moveZeros([1,2,312,11,2])			=>  [1,2,312,11,2]
+				
+				
+######################################################################### */
+
+
+function moveZeros(nums) 
+		{
+
+		// [ VARABLES ]__________________________
+			var pop = 0;
+			var arr_p = 0;
+			var zero_count = 0;
+		// _____________________________________
+
+			console.log('Orginal Array = ' +  nums);
+
+			for (var i = 0 ; i < nums.length ; i++)
+				{
+
+					// FOR LOOP TO CHECK FOR ZEROS
+						if (nums[i] == 0)
+							{
+								// COUNTS ALL THE ZEROS
+								pop = pop + 1;
+								// REMOVES ALL THE ZEROS FROM THE ARRAY
+								nums[i] = "";
+								// COUNTS ALL THE ZEROS THAT WERE REMOVED
+								zero_count = zero_count + 1;
+							}
+
+				}
+			
+				// FOR LOOP TO PUSH THE ZEROS TO THE END
+				for (j = zero_count; j > 0 ; j--)
+					{
+					// PUSHES ZEROS TO THE END OF THE ARRAY
+						nums.push(0);
+						
+					}
+
+				// FILTERS OUT ALL THE EMPTY SPACES OF THE ARRAY
+			nums_new = nums.filter(checkNum);
+
+			console.log('NEW Array  = ' +  nums_new);
+
+			
+		}
+
+			// check number
+function checkNum(value) {
+				return typeof value == 'number';
+			}
+
+
+
+moveZeros([5,8,0,78,6,8,4,0,0,7,8,0,5,3,0,6,5,,]) ;
+// moveZeros([0,1,3,12]) ;
